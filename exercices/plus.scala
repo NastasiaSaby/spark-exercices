48be4610304eb7271855ee3f53c560a05f140836
Nastasia Saby <nastasia.saby@gmail.com>
2018-12-10 18:42:37 +0100

    Create plus.scala

---
+++ exercices/plus.scala
@@ -0,0 +1,23 @@
+# Exercice 1
+
+- Récupérer les données de "customers" en CSV avec Sqoop
+- Avec Spark, trouver quel est l'Etat qui consomme le plus*
+- Créer une table Hive avec le résultat de ces données
+
+* Dans ce Spark, vous devez ajouter un schéma aux données entrantes pour avoir des noms autres que "_c1",...
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+val schema = StructType(
+      Array(
+          StructField("inputType",StringType,true), 
+          StructField("originalRating",LongType,true), 
+          StructField("processed",BooleanType,true), 
+          StructField("rating",LongType,true), 
+          StructField("score",DoubleType,true), 
+          StructField("methodId",StringType,true)
+      )
+  )
+            
+val  records =sqlContext.read.schema(schema).json("testData")
